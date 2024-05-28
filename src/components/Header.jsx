4b059a9eea9b703dee9e5565714d3b01323b3a82
slm55 <78506891+slm55@users.noreply.github.com>
2024-05-28 17:21:43 +0500

    header finished

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 function Header() {
   return (
@@ -10,11 +12,31 @@ function Header() {
             alt=""
           />
           <p className="text-3xl font-semibold text-gray-700 py-6">
-            Дарим <span className="text-white bg-green-500 rounded-full px-4 py-2 ">3000 ₸</span> и бесплатную доставку на первый заказ!
+            Дарим{" "}
+            <span className="text-white bg-green-500 rounded-full px-4 py-2 ">
+              3000 ₸
+            </span>{" "}
+            и бесплатную доставку на первый заказ!
           </p>
         </div>
       </div>
-      <div></div>
+      <div className="flex justify-between p-4 gap-48 items-center">
+        <img
+          src="https://arbuz.kz/static/platform/frontend/assets/banners/store_main.svg?v1"
+          alt="logo"
+          className="w-[180px]"
+        />
+        <div className="flex-1 flex rounded-lg bg-gray-100 p-2 gap-1">
+          <SearchIcon style={{color: "gray"}}/>
+          <input type="search" placeholder="Начните вводить название товара" className="flex-1 outline-none bg-transparent" />
+        </div>
+        <div className="flex gap-2">
+          <button className="bg-gray-100 rounded-lg py-2 px-3 text-gray-600">Войти</button>
+          <button className="bg-green-500 text-white p-1.5  rounded-lg ">
+            <ShoppingCartIcon /> 0
+          </button>
+        </div>
+      </div>
     </header>
   );
 }
